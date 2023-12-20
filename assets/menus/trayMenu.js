d8f1317e4b2958638ac5fa2d57728910d89094eb
Nimbi <JoshuaVL1988@protonmail.com>
2023-12-20 00:26:32 -0700

    :rewind: Revert changes back to previous build
    Revert all recent changes

--- assets/menus/trayMenu.js
+++
@@ -1,8 +0,0 @@
-import { app, Tray, Menu } from "electron";
-import { menuIcon } from '../configs/cfg';
-
-export const tray = new Tray(menuIcon);
-export const contextMenu = Menu.buildFromTemplate([
-  { label: "Open", type: "normal", click: () => app.show() },
-  { label: "Quit", type: "normal", click: () => app.quit() },
-]);
--- main.js
+++ main.js
@@ -1,39 +1,50 @@
-import { app, globalShortcut } from "electron";
-import { menubar } from "menubar";
+// Modules to control application life and create native browser window
+const { app, globalShortcut } = require("electron");
+const path = require("path");
+const { menubar } = require("menubar");
 
-import { tray, contextMenu } from "./assets/menus/trayMenu";
-import { siteUrl, menuIcon, persistPartition, commandSwitches, readyMessege } from "./assets/configs/cfg";
+const mb = menubar({
+  browserWindow: { width: 450, height: 660 },
+  preloadWindow: true,
+  icon: path.join(__dirname, "/MenuIcon.png"),
+  webPreferences: {
+    partition: "persist:youtubemusic",
+  },
+});
 
-app.on("ready", () => {
+mb.app.commandLine.appendSwitch(
+  "disable-backgrounding-occluded-windows",
+  "true"
+);
 
-  tray.setContextMenu(contextMenu);
+mb.on("ready", () => {
+  console.log("app is ready");
 
-  const mb = menubar({
-    tray,
-    browserWindow: {
-      width: 450,
-      height: 660,
-      webPreferences: {
-        partition: persistPartition,
-      },
-    },
-    preloadWindow: true,
-    icon: menuIcon,
-  });
+  win = mb.window;
+  // win.openDevTools();
 
-  mb.app.commandLine.appendSwitch(commandSwitches);
+  // First URL
+  win.loadURL("https://music.youtube.com/");
 
-  mb.on("ready", () => {
-    console.log(readyMessege);
+  // mb.on('after-create-window', () => {};
 
-    mb.window.loadURL(siteUrl);
+  // Once dom-ready
+  // win.webContents.once("dom-ready", () => {});
 
-    globalShortcut.register("CommandOrControl+X", () => {
-      if (process.platform !== "darwin") app.quit();
-    });
+  globalShortcut.register("CommandOrControl+X", () => {
+    if (process.platform !== "darwin") {
+      app.removeAllListeners();
+      app.quit();
+    }
   });
 });
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.removeAllListeners();
+    app.quit();
+  }
 });
